63b7feadae7a6d085d27ee2bd6bf3292b7073044
David Pick <pickdavid@gmail.com>
2011-01-20 14:28:12 -0500

    changing how pictures are stored

--- models.rb
+++ models.rb
@@ -26,7 +26,7 @@ class Photo
                     :s3_credentials => {:access_key_id => "AKIAIJUGMKYECWUFG2VQ",
                                         :secret_access_key => "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"},
                     :bucket => "mtm_test_data",
-                    :path => "#{Dir.pwd}/images/:id/:basename.:extension"
+                    :path => "images/:id/:basename.:extension"
 
   belongs_to :point
 end
