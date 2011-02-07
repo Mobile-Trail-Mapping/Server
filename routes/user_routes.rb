9f439aedca60c11cd79891c969e5cbf002cbb5c7
David Pick <pickdavid@gmail.com>
2011-02-06 20:24:12 -0500

    added user check route

--- routes/user_routes.rb
+++ routes/user_routes.rb
@@ -3,3 +3,9 @@ post '/user/add' do
   return "Added user #{user.email}"
 end
 
+post '/user/check' do
+  user = User.find(:email => params[:user], :pwhash => params[:pwhash])
+  return "true" if not user.nil?
+  return "false"
+end
+
