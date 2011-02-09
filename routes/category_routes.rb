96614b75f492e8c1bf42a149e9bd5a5f2566bc96
David Pick <pickdavid@gmail.com>
2011-02-09 16:04:41 -0500

    added /category/get route

--- routes/category_routes.rb
+++ routes/category_routes.rb
@@ -2,3 +2,9 @@ post '/category/add/?' do
   category = Category.first_or_create(:name => params[:category])
   "Added Category #{category.name}"
 end
+
+get '/category/get/?' do
+  @categories = Category.all
+
+  builder :category
+end
