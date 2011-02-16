ec1196a6c4cc227e37369827c624ce3da3dbd02c
David Pick <pickdavid@gmail.com>
2011-02-16 02:34:05 -0500

    added images to trail route

--- routes/trail_routes.rb
+++ routes/trail_routes.rb
@@ -32,6 +32,11 @@ end
 
 get '/trails/:trail/?' do
   @trail = Trail.first(:name => params[:trail])
+  @images = []
+  @trail.points.each do |point|
+    point.photos.each { |photo| @images << photo }
+  end
+
   haml :trail
 end
 
