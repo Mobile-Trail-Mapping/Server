a5aa6afe824f9716345bb38c0de74308e525e411
Pete Brousalis <brousapg@gmail.com>
2011-04-06 02:08:31 -0400

    Global.js cleaning

--- public/js/global.js
+++ public/js/global.js
@@ -86,52 +86,34 @@ $(document).ready(function() {
           $("#trail-coords").html(data.point.lat + ', ' + data.point.long);
           $("#trail-desc").html(data.point.desc)
           // pictures
-          $("ul#slider").html("");
-          $.each(data.point.photos, function(i) {
-            $("ul#slider").append('<li><img src="' + data.point.photos[i] + '" /></li>');
-          });       
+          if(data.point.photos) {  
+            $("ul#slider").html("");
+            $.each(data.point.photos, function(i) {
+              $("ul#slider").append('<li><img src="' + data.point.photos[i] + '" /></li>');
+            });       
              
-          $('#slider')
-          	.anythingSlider({
-          		startStopped        : true,
-          	})
-          	.anythingSliderFx({
-          		'.caption-top'    : [ 'caption-Top', '50px' ],
-          		'.caption-right'  : [ 'caption-Right', '130px' ],
-          		'.caption-bottom' : [ 'caption-Bottom', '50px' ],
-          		'.caption-left'   : [ 'caption-Left', '130px' ]
-          	})
-          	.find('div[class*=caption]')
-          		.css({ position: 'absolute' })
-          		.prepend('<span class="close">x</span>')
-          		.find('.close').click(function(){
-          			var cap = $(this).parent(),
-          				ani = { bottom : -50 }; // bottom
-          			if (cap.is('.caption-top')) { ani = { top: -50 }; }
-          			if (cap.is('.caption-left')) { ani = { left: -150 }; }
-          			if (cap.is('.caption-right')) { ani = { right: -150 }; }
-          			cap.animate(ani, 400, function(){ cap.hide(); } );
-          	});
-          // $(".slideshow").html(""); //clear pics for each point
-          // $.each(data.point.photos, function(i) {
-          //   $(".slideshow").append('<img src="' + data.point.photos[i] + '" />');
-          // });
-          // slideshow cycler
-          // $('.slideshow').cycle({
-          //   fx: 'fade', 
-          //   speed: 1000 , 
-          //   next: '.slideshow', 
-          //   timeout: 4000 
-          // });
-          // load the gmap
-          // $('#map_canvas').gMap({
-          //     controls: false,
-          //     scrollwheel: false,
-          //     markers: [{
-          //         latitude: data.point.lat,
-          //         longitude: data.point.long,
-          //     }]
-          // }); 
+            $('#slider')
+            	.anythingSlider({
+            		startStopped : true,
+            	})
+            	.anythingSliderFx({
+            		'.caption-top'    : [ 'caption-Top', '50px' ],
+            		'.caption-right'  : [ 'caption-Right', '130px' ],
+            		'.caption-bottom' : [ 'caption-Bottom', '50px' ],
+            		'.caption-left'   : [ 'caption-Left', '130px' ]
+            	})
+            	.find('div[class*=caption]')
+            		.css({ position: 'absolute' })
+            		.prepend('<span class="close">x</span>')
+            		.find('.close').click(function(){
+            			var cap = $(this).parent(),
+            				ani = { bottom : -50 }; // bottom
+            			if (cap.is('.caption-top')) { ani = { top: -50 }; }
+            			if (cap.is('.caption-left')) { ani = { left: -150 }; }
+            			if (cap.is('.caption-right')) { ani = { right: -150 }; }
+            			cap.animate(ani, 400, function(){ cap.hide(); } );
+            	});
+            }
         }
     });    
     
